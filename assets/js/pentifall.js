b96de508d29abc3dbde3632bf91b1a82c0b089f5
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-06-30 20:46:53 +0000

    Feat: add functionality to btns-display game area

---
+++ assets/js/pentifall.js
@@ -0,0 +1,17 @@
+// Start Menu
+const startMenu = document.getElementById('start-menu');
+const gameArea = document.getElementById('game-area');
+const playWithSoundBtn = document.getElementById('play=w-sound');
+const playWithoutSoundBtn = document.getElementById('play-w/o-sound');
+
+/**
+ * Start the game when one of the two buttons are clicked.
+ * One instance with sound, one without sound.
+ */
+function startGame() {
+    startMenu.style.display = 'none';
+    gameArea.style.display = 'flex';
+}
+
+playWithSoundBtn.addEventListener('click', startGame);
+playWithoutSoundBtn.addEventListener('click', startGame);
