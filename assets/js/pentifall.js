3aece30c94d748f15d34e75135cc5f29e754f29f
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-06-30 19:44:26 +0000

    Feat: change message depending on different scenerios

--- assets/js/pentifall.js
+++ assets/js/pentifall.js
@@ -1,13 +1,21 @@
 /**
  * Change default error messages for input fields.
  * Anonymous function to change default error message when nothing is entered into field.
+ * Anonymous function to change default error message when less than 3 characters have been entered.
+ * Anonymous function to change default error message on pattern mismatch.
  */
-function handleInput () {
+function handleInput() {
     let inputElement = document.getElementById('player-name');
 
     inputElement.oninvalid = function () {
         if (this.validity.valueMissing) {
             this.setCustomValidity('Please enter your name.');
+        } else if (this.validity.tooShort) {
+            this.setCustomValidity('Please enter 3 characters of more.');
+        } else if (this.validity.patternMismatch) {
+            this.setCustomValidity('Please only use letters!');
+        } else {
+            this.setCustomValidity('');
         }
     };
 };
