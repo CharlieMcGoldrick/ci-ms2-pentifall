532db4d299ca0d200c330349bd58ae534009f281
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-06-30 21:28:27 +0000

    Feat: add error messages to function & log them

--- assets/js/pentifall.js
+++ assets/js/pentifall.js
@@ -3,7 +3,7 @@ const startMenu = document.getElementById('start-menu');
 const gameArea = document.getElementById('game-area');
 const playWithSoundBtn = document.getElementById('play-w-sound');
 const playWithoutSoundBtn = document.getElementById('play-w/o-sound');
-const playerNameInput = document.getElementById('player-name');
+const playerNameInput = document.getElementById('player-name'); // Capture playername for validation & end screen score display 
 
 /**
  * Start the game when one of the two buttons are clicked.
@@ -11,14 +11,23 @@ const playerNameInput = document.getElementById('player-name');
  */
 function startGame() {
     const playerName = playerNameInput.value.trim(); // Trim any whitespace 
-    if (playerName.length >= 3 && /^[A-Za-z]+$/.test(playerName)) {
+    if (playerName.length >= 3 && /^[A-Za-z]+$/.test(playerName)) { // Test name input to be more than 3 characters and using the desired pattern
         // Valid input, hide the start menu and show the game area
         startMenu.style.display = 'none';
         gameArea.style.display = 'flex';
 
         // Game logic goes here
     } else {
-        // Error Message goes here 
+        let errorMessage = '';
+
+        if (playerName.length < 3) {
+            errorMessage = 'Please enter 3 characters or more!';
+        } else if (!/^[A-Za-z]+$/.test(playerName)) {
+            errorMessage = 'Please only use letters!';
+        } else {
+            errorMessage = 'Please enter a valid name!';
+        }
+        console.log(errorMessage);
     }
 }
 
