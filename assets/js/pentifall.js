eadbc88a6a6fd231ba6ee2a4df8293820efcb00e
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-06-30 21:21:06 +0000

    Feat: change to validate  in startGame function

--- assets/js/pentifall.js
+++ assets/js/pentifall.js
@@ -3,14 +3,23 @@ const startMenu = document.getElementById('start-menu');
 const gameArea = document.getElementById('game-area');
 const playWithSoundBtn = document.getElementById('play-w-sound');
 const playWithoutSoundBtn = document.getElementById('play-w/o-sound');
+const playerNameInput = document.getElementById('player-name');
 
 /**
  * Start the game when one of the two buttons are clicked.
  * One instance with sound, one without sound.
  */
 function startGame() {
-    startMenu.style.display = 'none';
-    gameArea.style.display = 'flex';
+    const playerName = playerNameInput.value.trim(); // Trim any whitespace 
+    if (playerName.length >= 3 && /^[A-Za-z]+$/.test(playerName)) {
+        // Valid input, hide the start menu and show the game area
+        startMenu.style.display = 'none';
+        gameArea.style.display = 'flex';
+
+        // Game logic goes here
+    } else {
+        // Error Message goes here 
+    }
 }
 
 playWithSoundBtn.addEventListener('click', startGame);
